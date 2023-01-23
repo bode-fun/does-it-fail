d4b3abc11f093291a7cde1b39c83174112260613
Jan Fooken <git@bode.fun>
2023-01-23 12:58:13 +0100

    Update the comments in the swift example

--- swift/Sources/ReadFile/ReadFile.swift
+++ swift/Sources/ReadFile/ReadFile.swift
@@ -3,19 +3,39 @@ import Foundation
 @main
 public struct ReadFile {
   public static func main() {
-    // A try expresion has to be wrapped in a do-catch block
-    // or the function has to be marked as throws
+  // public static func main() throws {
+
+    // To handle functions which can throw errors, one uses the "try" keyword.
+    // Try expressions can be handled in two ways:
+    // One can either use a do-catch block
+    // or mark the function signature of the caller (this method) 
+    // with the keyword "throws" (see above).
+    // Swift forces one to handle errors.
+    // Try to modify the code and see how the compiler reacts.
+    // Spoiler: the program will not compile if a possible error is not handled.
+    // Try to do the following:
+    // 1. Remove the try keyword, compile the program
+    // 2. Put it back and remove the do-catch block, compile the program
+    // 3. Change the method signature to throws (see above),
+    //    remove the do-catch block and compile the program
     do {
       try printFileLineByLine(path: "../README.md")
     } catch {
+      // Print the error
       print("Error: \(error)")
     }
+
+    // This example shows only one way to handle errors.
+    // The main takeaway is that compared to other languages,
+    // Swift forces one to handle errors at compile time.
+    // This increases the reliability of the code and adds safety.
   }
 
   public static func printFileLineByLine(path: String) throws {
-    // Read file contents into a string
     // Try to remove the try keyword and see what happens
     // The program will not compile if a possible error is not handled
+    
+    // Read file contents into a string
     let fileURL = URL(fileURLWithPath: path)
     let fileContents = try String(contentsOf: fileURL, encoding: .utf8)
     
